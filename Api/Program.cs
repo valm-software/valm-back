294397a2892e501b21ecbe2b0e3f4e4cfc2c3ba5
MSI\valm-software <valm.software@hotmail.com>
2023-09-04 14:08:37 +0200

    update puerto 2

--- Api/Program.cs
+++ Api/Program.cs
@@ -5,15 +5,13 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 // Add services to the container.
-
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 builder.Services.ConfigureCors();
 builder.Services.AddAplicacionServices();
-
 builder.Services.AddControllers();
 
-    // Este es actualizado y autodetecta la version de mariaDb
+// Este es actualizado y autodetecta la version de mariaDb
 builder.Services.AddDbContext<ValmContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("MariaDBConnection");
@@ -31,10 +29,15 @@ builder.Services.AddDbContext<ValmContext>(options =>
 });   
 */
 
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configurar el puerto 5000
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5000);
+});
 
 var app = builder.Build();
 
@@ -48,11 +51,11 @@ if (app.Environment.IsDevelopment())
 // Aplicamos de manera automatica las actualizaciones de la base de datos
 using (var scope = app.Services.CreateScope())
 {
-    var services =scope.ServiceProvider;
-    var loggerfactory =services.GetRequiredService<ILoggerFactory>();
+    var services = scope.ServiceProvider;
+    var loggerfactory = services.GetRequiredService<ILoggerFactory>();
     try
     {
-        var context =services.GetRequiredService<ValmContext>();
+        var context = services.GetRequiredService<ValmContext>();
         await context.Database.MigrateAsync();
         await ValmContextSeed.SeedAsync(context, loggerfactory);
     }
